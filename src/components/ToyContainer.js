3905dee8fc6b8eeb50e215ce907de8095b36d102
Enkhbat Munkhbold <emunkhbold10@gmail.com>
2022-05-05 11:03:25 -0700

    Initialize rendering cards

--- src/components/ToyContainer.js
+++ src/components/ToyContainer.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ToyCard from "./ToyCard";
 
 function ToyContainer() {
+  const [cards, setCards] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:3001/toys")
+    .then(res => res.json())
+    .then(data => setCards(data))
+  }, [])
+
+  const renderCards = () => {
+    return cards.map(c => {
+      return <ToyCard key={c.id} card={c}/>
+    })
+  }
   return (
-    <div id="toy-collection">{/* Render the collection of ToyCards */}</div>
+    <div id="toy-collection">{renderCards()}</div>
   );
 }
 
